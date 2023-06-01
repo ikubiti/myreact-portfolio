effd23a84c57fc805b67d8aabfbc1909c73b850d
Isoyor Kubiti <ikubiti@icloud.com>
2023-06-01 00:30:27 -0400

    modified contact form and added updated resume file

--- src/components/pages/Resume.js
+++ src/components/pages/Resume.js
@@ -1,32 +1,32 @@
 import React from 'react';
-import resume from '../../utils/resume_isoyor.pdf';
+import resume from '../../images/Resume_Isoyor_Kubiti.pdf';
 
 const proficiencies = require('../../utils/proficiencies.json');
 
 export default function Blog() {
-	return (
-		<div className='container contact mb-5'>
-			<h1>
-				<a
-					className='m-3'
-					href={resume}
-					target='_blank'
-					rel='noreferrer'
-					download
-				>
-					Resume
-				</a>
-			</h1>
+    return (
+        <div className='container contact mb-5'>
+            <h1>
+                <a
+                    className='m-3'
+                    href={resume}
+                    target='_blank'
+                    rel='noreferrer'
+                    download
+                >
+                    Resume
+                </a>
+            </h1>
 
-			<ul className='list-group list-group-flush'>
-				{proficiencies.map((proficiency, index) => {
-					return (
-						<li className='list-group-item' key={index}>
-							{proficiency}
-						</li>
-					);
-				})}
-			</ul>
-		</div>
-	);
+            <ul className='list-group list-group-flush'>
+                {proficiencies.map((proficiency, index) => {
+                    return (
+                        <li className='list-group-item' key={index}>
+                            {proficiency}
+                        </li>
+                    );
+                })}
+            </ul>
+        </div>
+    );
 }
